ceb40491e1b2214235661f8ca6258cb3eb67df2b
Bruce <irabruce10@gmail.com>
2024-01-30 12:01:12 +0100

    Initialize project using create React app

--- src/App.js
+++ src/App.js
@@ -33,6 +33,12 @@ function Button({ children, onClick }) {
 export default function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [product, setProduct] = useState(initialFriends);
+  const [select, setSelect] = useState(null);
+
+  function handleSelect(product) {
+    setSelect(product);
+    console.log(select);
+  }
 
   function handleToggle() {
     setIsOpen(!isOpen);
@@ -40,20 +46,23 @@ export default function App() {
 
   function handleNewProduct(product) {
     setProduct((products) => [...products, product]);
+    setIsOpen(false);
   }
 
   return (
     <div className="app">
       <div className="sidebar">
-        <List product={product} />
-        {/* onNewProduct={handleNewProduct} */}
-        {isOpen && <Form />}
+        <List product={product} onSelect={handleSelect} />
+
+        {isOpen && <Form onNewProduct={handleNewProduct} />}
 
         <Button onClick={handleToggle}>
           {isOpen ? "Close" : "Add Product"}
         </Button>
       </div>
       {/* <Header /> */}
+
+      {select && <ProductDetail select={select} />}
     </div>
   );
 }
@@ -62,24 +71,24 @@ function Header() {
   return <h1>My shopping list.....</h1>;
 }
 
-function List({ product }) {
+function List({ product, onSelect }) {
   return (
     <ul>
       {product.map((product) => (
-        <Product key={product.id} product={product} />
+        <Product key={product.id} product={product} onSelect={onSelect} />
       ))}
     </ul>
   );
 }
 
-function Product({ product }) {
+function Product({ product, onSelect }) {
   return (
     <div>
       <li>
         <img src={product.image} alt={product.image} />
         <h3>{product.name}</h3>
         <p>{product.balance}</p>
-        <Button>Select</Button>
+        <Button onClick={() => onSelect(product)}>Select</Button>
       </li>
     </div>
   );
@@ -87,13 +96,16 @@ function Product({ product }) {
 
 function Form({ onNewProduct }) {
   const [name, setName] = useState("");
-  const [image, setImage] = useState("");
+  const [image, setImage] = useState("https://i.pravatar.cc/48");
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    // const newProduct = { name, balance, image, id };
-    setImage("");
+    let id = crypto.randomUUID();
+    const newProduct = { name, image: `${image}?=${id}`, id: { id } };
+    onNewProduct(newProduct);
+
+    setImage("https://i.pravatar.cc/48");
     setName("");
   }
 
@@ -109,6 +121,14 @@ function Form({ onNewProduct }) {
   );
 }
 
-function ProductDetail() {}
+function ProductDetail({ select }) {
+  return (
+    <div className="form-split-bill">
+      <h2>my name is {select.name} </h2>
+      <img src={select.image} alt={select.image} />
+      <p>{select.balance}</p>
+    </div>
+  );
+}
 
 function Cart() {}
